1f9021f88ac1427ba8e196c7a5574edf62c5ec6a
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-13 14:27:29 +0300

    Add files via upload

---
+++ HibernateSpringBootDataSourceProxy/src/main/java/com/jpa/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    public User findByName(String name);
+}
