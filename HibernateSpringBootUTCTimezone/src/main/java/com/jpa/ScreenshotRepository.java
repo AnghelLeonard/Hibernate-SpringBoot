d4023cfdf8f7ef7fcb4cce4461d50b653688a7ca
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-14 16:33:47 +0300

    Update ScreenshotRepository.java

--- HibernateSpringBootUTCTimezone/src/main/java/com/jpa/ScreenshotRepository.java
+++ HibernateSpringBootUTCTimezone/src/main/java/com/jpa/ScreenshotRepository.java
@@ -2,9 +2,11 @@ package com.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ScreenshotRepository extends JpaRepository<Screenshot, Long> {
     
+    @Transactional(readOnly=true)
     public Screenshot findByName(String name);
 }
