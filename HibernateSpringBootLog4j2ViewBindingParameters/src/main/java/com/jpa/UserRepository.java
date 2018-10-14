a3c214c176f594a88835eb5aae043d2f0a323078
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-14 16:33:02 +0300

    Update UserRepository.java

--- HibernateSpringBootLog4j2ViewBindingParameters/src/main/java/com/jpa/UserRepository.java
+++ HibernateSpringBootLog4j2ViewBindingParameters/src/main/java/com/jpa/UserRepository.java
@@ -2,9 +2,11 @@ package com.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
+    @Transactional(readOnly=true)
     public User findByName(String name);
 }
