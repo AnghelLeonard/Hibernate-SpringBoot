365c19598b17350675e05b8e1c30861b9acc0e1c
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-22 10:15:09 +0300

    Update UserRepository.java

--- HibernateSpringBootQueryFetching/src/main/java/com/jpa/UserRepository.java
+++ HibernateSpringBootQueryFetching/src/main/java/com/jpa/UserRepository.java
@@ -2,6 +2,7 @@ package com.jpa;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +11,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
     @Transactional(readOnly = true)
     List<User> findByName(String name);
+    
+    @Transactional(readOnly = true)
+    @Query("select u from User u where u.name = ?1")
+    List<User> fetchUsersByName(String name);
 }
 
