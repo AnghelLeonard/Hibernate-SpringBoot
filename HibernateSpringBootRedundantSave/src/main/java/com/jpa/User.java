09c0599be25b4ec25522ba4c735131dd95332d70
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-22 15:45:12 +0300

    Add files via upload

---
+++ HibernateSpringBootRedundantSave/src/main/java/com/jpa/User.java
@@ -0,0 +1,36 @@
+package com.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;   
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
