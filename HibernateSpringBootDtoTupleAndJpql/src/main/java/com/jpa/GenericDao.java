3e1623c30202fd7811b606147ec2c27fe6fb6eef
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-25 12:03:34 +0300

    Update GenericDao.java

--- HibernateSpringBootDtoTupleAndJpql/src/main/java/com/jpa/GenericDao.java
+++ HibernateSpringBootDtoTupleAndJpql/src/main/java/com/jpa/GenericDao.java
@@ -1,9 +1,9 @@
 package com.jpa;
 
 import java.io.Serializable;
-import java.util.List;
 
 public interface GenericDao<T, ID extends Serializable> {
 
     <S extends T> S persist(S entity);        
 }
+
