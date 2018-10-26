a2172feec14c3b3aa6d4d00c9cbe66b21ef0e852
AnghelLeonard <leoprivacy@yahoo.com>
2018-10-26 10:02:42 +0300

    Update CarDtoWithSetters.java

--- HibernateSpringBootDtoResultTransformer/src/main/java/com/jpa/CarDtoWithSetters.java
+++ HibernateSpringBootDtoResultTransformer/src/main/java/com/jpa/CarDtoWithSetters.java
@@ -1,6 +1,10 @@
 package com.jpa;
 
-public class CarDtoWithSetters {
+import java.io.Serializable;
+
+public class CarDtoWithSetters implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     
     private String name;
     private String color;    
