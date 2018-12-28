d8caf601268de81f92134b3e22b67dda433600f0
AnghelLeonard <leoprivacy@yahoo.com>
2018-12-28 09:59:25 +0200

    Update ConfigureDataSource.java

--- HibernateSpringBootDataSourceBuilderHikariCPKickoff/src/main/java/com/jpa/ConfigureDataSource.java
+++ HibernateSpringBootDataSourceBuilderHikariCPKickoff/src/main/java/com/jpa/ConfigureDataSource.java
@@ -1,17 +1,26 @@
 package com.jpa;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ConfigureDataSource {
 
     @Bean
+    @Primary
     @ConfigurationProperties("app.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class)
+                .build();
     }
 }
