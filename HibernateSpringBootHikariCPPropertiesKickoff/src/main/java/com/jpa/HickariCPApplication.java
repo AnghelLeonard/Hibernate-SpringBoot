9d233a77f8eaf4d512de5b4c00d533637d66df78
AnghelLeonard <leoprivacy@yahoo.com>
2018-12-28 09:14:36 +0200

    Add files via upload

---
+++ HibernateSpringBootHikariCPPropertiesKickoff/src/main/java/com/jpa/HickariCPApplication.java
@@ -0,0 +1,13 @@
+package com.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HickariCPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HickariCPApplication.class, args);
+    }
+
+}
