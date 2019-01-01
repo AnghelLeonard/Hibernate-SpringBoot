c0aad8159e83964e259ae38d43fe0dd611615416
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-01 12:15:12 +0200

    Update C3P0Application.java

--- HibernateSpringBootDataSourceBuilderC3P0Kickoff/src/main/java/com/jpa/C3P0Application.java
+++ HibernateSpringBootDataSourceBuilderC3P0Kickoff/src/main/java/com/jpa/C3P0Application.java
@@ -3,6 +3,9 @@ package com.jpa;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +16,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class C3P0Application {
 
+    private static final Logger logger = Logger.getLogger(C3P0Application.class.getName());   
+    
     private static final ExecutorService executor = Executors.newFixedThreadPool(25);
 
     @Autowired
     private ApplicationContext applicationContext;    
+    
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(C3P0Application.class, args);
@@ -26,6 +34,10 @@ public class C3P0Application {
     public ApplicationRunner init() {
         return args -> {
 
+            logger.info("-------------------------------------------------------");
+            logger.log(Level.INFO, "DataSource: {0}", dataSource);
+            logger.info("-------------------------------------------------------");
+            
             while(true) {
                 SampleRepository sampleThread
                         = applicationContext.getBean(SampleRepository.class);
