8b5d0e344546a7e191858f63bb6ab9314adbe193
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-01 11:52:42 +0200

    Add files via upload

---
+++ HibernateSpringBootDataSourceBuilderTomcatKickoff/src/main/java/com/jpa/TomcatApplication.java
@@ -0,0 +1,51 @@
+package com.jpa;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TomcatApplication {
+
+    private static final Logger logger = Logger.getLogger(TomcatApplication.class.getName());
+
+    private static final ExecutorService executor = Executors.newFixedThreadPool(25);
+
+    @Autowired
+    private ApplicationContext applicationContext;    
+    
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            logger.info("-------------------------------------------------------");
+            logger.log(Level.INFO, "DataSource: {0}", dataSource);
+            logger.info("-------------------------------------------------------");
+            
+            while(true) {
+                SampleRepository sampleThread
+                        = applicationContext.getBean(SampleRepository.class);
+                executor.execute(sampleThread);
+                
+                Thread.sleep(new Random().nextInt(125));
+            }
+
+        };
+    }
+}
