c2d50ecf9feaa809fa1eb835880f89bba9108923
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-08 12:09:15 +0200

    Add files via upload

---
+++ HibernateSpringBootSliceAllCriteriaBuilderAndSort/src/main/java/com/jpa/service/PlayerService.java
@@ -0,0 +1,23 @@
+package com.jpa.service;
+
+import com.jpa.domain.Player;
+import com.jpa.repository.PlayerRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService {
+     
+    private final PlayerRepository playerRepository;
+    
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+            
+    public Slice<Player> fetchNextSlice(int page, int size) {
+               
+        return playerRepository.findAll(PageRequest.of(page, size, new Sort(Sort.Direction.ASC, "name")));
+    }
+}
