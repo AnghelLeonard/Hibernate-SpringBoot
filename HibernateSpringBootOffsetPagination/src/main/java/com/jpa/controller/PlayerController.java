cfba95fb3787b9eb22eaf1191179c0d975208a0c
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-10 09:48:04 +0200

    Update PlayerController.java

--- HibernateSpringBootOffsetPagination/src/main/java/com/jpa/controller/PlayerController.java
+++ HibernateSpringBootOffsetPagination/src/main/java/com/jpa/controller/PlayerController.java
@@ -2,7 +2,7 @@ package com.jpa.controller;
 
 import com.jpa.domain.Player;
 import com.jpa.service.PlayerService;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class PlayerController {
     }
 
     @GetMapping("/players/{page}/{size}")
-    public Slice<Player> fetchPlayersByPageAndSizePath(@PathVariable int page, @PathVariable int size) {
+    public Page<Player> fetchPlayersByPageAndSizePath(@PathVariable int page, @PathVariable int size) {
 
         return playerService.fetchNextSlice(page, size);
     }
