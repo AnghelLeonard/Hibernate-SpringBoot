88be5c42317790033a76c28c06222ccd8beb8d66
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-12 11:40:36 +0200

    Add files via upload

---
+++ HibernateSpringBootBatchInsertOrder/src/main/java/com/jpa/BatchInsertOrderApplication.java
@@ -0,0 +1,26 @@
+package com.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BatchInsertOrderApplication {
+
+    @Autowired
+    private TennisArenaService tennisArenaService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchInsertOrderApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            
+            tennisArenaService.insertPlayers();
+        };
+    }
+}
---
+++ HibernateSpringBootBatchInsertOrder/src/main/java/com/jpa/TennisPlayer.java
@@ -0,0 +1,63 @@
+package com.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class TennisPlayer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id    
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tournament_id")
+    private Tournament tournament;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Tournament getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof TennisPlayer)) {
+            return false;
+        }
+        return id != null && id.equals(((TennisPlayer) obj).id);        
+    }
+
+    @Override
+    public int hashCode() {
+        return 2018;
+    }
+}
