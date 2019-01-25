dcafa1924ca187da45dda544e4c78e24c64e1711
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-25 11:33:49 +0200

    Add files via upload

---
+++ HibernateSpringBootDtoViaProjectionsAndJpql/src/main/java/com/jpa/User.java
@@ -0,0 +1,80 @@
+package com.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String city;
+    private String country;
+    private long ssn;
+    private int age;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }           
+}
---
+++ HibernateSpringBootDtoViaProjectionsAndJpql/src/main/java/com/jpa/UserNameAndCity.java
@@ -0,0 +1,7 @@
+package com.jpa;
+
+public interface UserNameAndCity {
+    
+    String getName();
+    String getCity();
+}
