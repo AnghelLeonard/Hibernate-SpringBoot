b0fd43572518b3f84d96537ea3b44a44d67f87ec
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-27 17:14:37 +0200

    Add files via upload

---
+++ HibernateSpringBootMatchSchemaAndTable/src/main/resources/schema-mysql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `db_cars`;
+CREATE TABLE IF NOT EXISTS `db_cars`.`lux_cars` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE DATABASE IF NOT EXISTS `db_drivers`;
+CREATE TABLE IF NOT EXISTS `db_drivers`.`lux_drivers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
