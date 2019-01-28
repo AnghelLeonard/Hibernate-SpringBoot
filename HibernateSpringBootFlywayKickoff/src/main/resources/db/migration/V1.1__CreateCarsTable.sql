22976b03175b0bb6714982c6390f3efc2d3d35e1
AnghelLeonard <leoprivacy@yahoo.com>
2019-01-28 14:50:17 +0200

    Add files via upload

---
+++ HibernateSpringBootFlywayKickoff/src/main/resources/db/migration/V1.1__CreateCarsTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `db_cars`.`lux_cars` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
