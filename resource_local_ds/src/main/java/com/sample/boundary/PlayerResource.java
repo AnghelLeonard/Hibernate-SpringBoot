f691c6364fbd4b0322dcecd4428a69650d2aa490
AnghelLeonard <leoprivacy@yahoo.com>
2019-02-03 09:03:15 +0200

    Add files via upload

---
+++ resource_local_ds/src/main/java/com/sample/boundary/PlayerResource.java
@@ -0,0 +1,50 @@
+package com.sample.boundary;
+
+import com.sample.model.Player;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("player")
+public class PlayerResource {
+
+    private static final Logger logger = Logger.getLogger(PlayerResource.class.getName());
+    
+    @POST
+    public String generatePlayer() {
+
+        Player player = new Player();
+        player.setName("Some Name");
+        player.setCity("Some city");
+        player.setAge(20);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(player);
+            tx.commit();
+
+            return "Success";
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Exception: {0}", e);
+
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+        } finally {
+            if (em.isOpen()) {               
+                em.close();
+            }          
+        }
+
+        return "Failure";
+    }
+}
