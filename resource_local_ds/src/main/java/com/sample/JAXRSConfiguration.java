1c44bc3ccde638315ab064ce2f53b904aeefe2a6
AnghelLeonard <leoprivacy@yahoo.com>
2019-02-05 09:12:11 +0200

    Delete JAXRSConfiguration.java

--- resource_local_ds/src/main/java/com/sample/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.sample;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
