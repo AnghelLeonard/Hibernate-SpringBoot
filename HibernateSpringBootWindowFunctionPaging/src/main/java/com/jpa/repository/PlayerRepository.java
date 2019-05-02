cb66e7af415590eadf23299c8ca873d6f966b975
AnghelLeonard <leoprivacy@yahoo.com>
2019-05-02 16:16:05 +0300

    Update PlayerRepository.java

--- HibernateSpringBootWindowFunctionPaging/src/main/java/com/jpa/repository/PlayerRepository.java
+++ HibernateSpringBootWindowFunctionPaging/src/main/java/com/jpa/repository/PlayerRepository.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
+    @Transactional(readOnly=true)
     @Query(value = "SELECT name, age, COUNT(*) OVER() AS total FROM player ORDER BY age LIMIT ?1, ?2",
             nativeQuery = true)
     List<PlayerDto> fetchAllPlayers(int page, int size);
