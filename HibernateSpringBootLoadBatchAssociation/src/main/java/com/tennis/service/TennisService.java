f897b610199a0ac8aa97576d46b891731acd35e1
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-02 10:06:30 +0300

    Update TennisService.java

--- HibernateSpringBootLoadBatchAssociation/src/main/java/com/tennis/service/TennisService.java
+++ HibernateSpringBootLoadBatchAssociation/src/main/java/com/tennis/service/TennisService.java
@@ -12,7 +12,6 @@ public class TennisService {
     private final TournamentRepository tournamentRepository;
     
     public TennisService(TournamentRepository tournamentRepository) {
-
         this.tournamentRepository = tournamentRepository;
     }
     
