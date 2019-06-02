fa5cc278232545bb1d7dce41ae12c837dfa7b2e4
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-02 10:05:41 +0300

    Fetch association in batches

--- HibernateSpringBootLoadBatchAssociation/src/main/java/com/tennis/service/TennisService.java
+++ HibernateSpringBootLoadBatchAssociation/src/main/java/com/tennis/service/TennisService.java
@@ -3,16 +3,12 @@ package com.tennis.service;
 import com.tennis.repository.TournamentRepository;
 import com.tennis.entity.Tournament;
 import java.util.List;
-import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TennisService {
-
-    private static final Logger logger
-            = Logger.getLogger(TennisService.class.getName());
-
+ 
     private final TournamentRepository tournamentRepository;
     
     public TennisService(TournamentRepository tournamentRepository) {
