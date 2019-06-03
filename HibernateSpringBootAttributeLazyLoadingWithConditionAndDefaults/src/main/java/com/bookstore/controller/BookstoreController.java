cc334acd9c25b673596284e412543ef9a072e26f
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-03 15:46:05 +0300

    Attribute Lazy Loading

--- HibernateSpringBootAttributeLazyLoadingWithConditionAndDefaults/src/main/java/com/bookstore/controller/BookstoreController.java
+++ HibernateSpringBootAttributeLazyLoadingWithConditionAndDefaults/src/main/java/com/bookstore/controller/BookstoreController.java
@@ -2,14 +2,9 @@ package com.bookstore.controller;
 
 import com.bookstore.service.BookstoreService;
 import com.bookstore.entity.Author;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
