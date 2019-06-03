8fbad3c1b4c338f41c40ff4ada68787e6b01d6af
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-03 18:35:38 +0300

    Skip Locked PostgreSQL

---
+++ HibernateSpringBootPostgresSqlSkipLocked/src/main/java/com/app/entity/ArticleStatus.java
@@ -0,0 +1,7 @@
+package com.app.entity;
+
+public enum ArticleStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ HibernateSpringBootPostgresSqlSkipLocked/src/main/resources/data-postgresql.sql
@@ -0,0 +1,5 @@
+INSERT INTO article (id, content, status, title) VALUES ('1', 'content_1', 'PENDING', 'title_1');
+INSERT INTO article (id, content, status, title) VALUES ('2', 'content_2', 'PENDING', 'title_2');
+INSERT INTO article (id, content, status, title) VALUES ('3', 'content_3', 'PENDING', 'title_3');
+INSERT INTO article (id, content, status, title) VALUES ('4', 'content_4', 'PENDING', 'title_4');
+INSERT INTO article (id, content, status, title) VALUES ('5', 'content_5', 'PENDING', 'title_5');
