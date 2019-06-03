cbeadd4d315f968b9c769a328d0697643e5585b3
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-03 14:46:31 +0300

    Hibernate5Module

---
+++ HibernateSpringBootJacksonHibernate5Module/src/main/java/com/tennis/repository/TournamentRepository.java
@@ -0,0 +1,12 @@
+package com.tennis.repository;
+
+import com.tennis.entity.Tournament;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> { 
+    
+    Tournament findByName(String name);
+}
+
---
+++ HibernateSpringBootJacksonHibernate5Module/src/main/resources/data-mysql.sql
@@ -0,0 +1,9 @@
+INSERT INTO tournament (id, name, location) VALUES (1, 'Roland Garros', 'France');
+INSERT INTO tournament (id, name, location) VALUES (2, 'US Open', 'United States');
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (1, 'Rafael Nadal', 1);
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (2, 'Roger Federer', 1);
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (3, 'David Ferer', 2);
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (4, 'Andy Murray', 2);
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (5, 'Del Potro', 2);
+INSERT INTO tennis_player (id, name, tournament_id) VALUES (6, 'Novak D', 2);
+
