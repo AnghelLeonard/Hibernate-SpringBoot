066e57681902538daa99bdfc60f6b58bc19220ed
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-04 16:26:46 +0300

    Attribute Lazy Loading

--- HibernateSpringBootAttributeLazyLoadingWithConditionAndDefaults/src/main/java/com/bookstore/controller/BookstoreController.java
+++ HibernateSpringBootAttributeLazyLoadingWithConditionAndDefaults/src/main/java/com/bookstore/controller/BookstoreController.java
@@ -2,7 +2,6 @@ package com.bookstore.controller;
 
 import com.bookstore.service.BookstoreService;
 import com.bookstore.entity.Author;
-import java.io.IOException;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +13,6 @@ public class BookstoreController {
 
     public BookstoreController(BookstoreService bookstoreService) {
         this.bookstoreService = bookstoreService;
-    }
-
-    @GetMapping("/create")
-    public String create() throws IOException {
-
-        bookstoreService.createAuthors();
-
-        return "created";
     }
 
     @GetMapping("/authors")
