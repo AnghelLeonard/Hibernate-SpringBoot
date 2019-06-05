4646ef4e828a618bf13d58319961add4bafd0862
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-05 12:08:33 +0300

    Attribute Lazy Loading

--- HibernateSpringBootAttributeLazyLoadingBasic/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootAttributeLazyLoadingBasic/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional(readOnly=true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {    
     
+    @Transactional(readOnly=true)
     List<Author> findByAgeGreaterThanEqual(int age);
 }
 
