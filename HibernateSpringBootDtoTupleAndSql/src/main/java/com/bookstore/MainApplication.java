dd2e9e11f986e7c7bf27193917e8025306335bce
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-06 11:03:48 +0300

    Dto Via Tuple And Native SQL

---
+++ HibernateSpringBootDtoTupleAndSql/src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,38 @@
+package com.bookstore;
+
+import java.util.List;
+import com.bookstore.service.BookstoreService;
+import javax.persistence.Tuple;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            List<Tuple> authors = bookstoreService.fetchAuthors();
+
+            System.out.println("Number of authors:" + authors.size());
+
+            for (Tuple author : authors) {
+                System.out.println("Author name: " + author.get("name")
+                        + " | Age: " + author.get("age"));
+            }
+        };
+    }
+}
---
+++ HibernateSpringBootDtoTupleAndSql/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,18 @@
+package com.bookstore.repository;
+
+import java.util.List;
+import com.bookstore.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.Tuple;
+import org.springframework.data.jpa.repository.Query;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Transactional(readOnly = true)
+    @Query(value = "select name, age from author",
+            nativeQuery = true)
+    List<Tuple> fetchAuthors();
+}
