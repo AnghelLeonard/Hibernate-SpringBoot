b78f84cd22337fadfc9292bb4e8c947628fa5caf
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-06 14:17:03 +0300

    Attributes Lazy Loading Via Subentites

--- HibernateSpringBootSubentities/src/main/java/com/bookstore/repository/AuthorShallowRepository.java
+++ HibernateSpringBootSubentities/src/main/java/com/bookstore/repository/AuthorShallowRepository.java
@@ -1,15 +1,10 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.AuthorShallow;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AuthorShallowRepository extends JpaRepository<AuthorShallow, Long> {   
-    
-    @Transactional(readOnly=true)
-    List<AuthorShallow> findByAgeGreaterThanEqual(int age);
+public interface AuthorShallowRepository extends JpaRepository<AuthorShallow, Long> {       
 }
 
