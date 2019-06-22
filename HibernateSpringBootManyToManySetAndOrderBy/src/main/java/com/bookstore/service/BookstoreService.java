88dfaeec048af7119ae1f01de5b94754adbc3d60
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-22 07:28:01 +0300

    Many-to-many Set and Order By

---
+++ HibernateSpringBootManyToManySetAndOrderBy/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,27 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.entity.Book;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.bookstore.repository.BookRepository;
+import java.util.Set;
+
+@Service
+public class BookstoreService {
+
+    private final BookRepository bookRepository;
+
+    public BookstoreService(BookRepository bookRepository) {
+
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public void fetchAuthorsOfBook() {
+        Book book = bookRepository.findById(1L).orElseThrow();
+        Set<Author> authorsSet = book.getAuthors();
+
+        System.out.println("Books ordered descending:\n" + authorsSet);
+    }
+}
---
+++ HibernateSpringBootManyToManySetAndOrderBy/src/main/resources/data-mysql.sql
@@ -0,0 +1,15 @@
+insert into author (age, name, genre, id) values (23, "Mark Janel", "Anthology", 1);
+insert into author (age, name, genre, id) values (51, "Quartis Young", "Anthology", 2);
+insert into author (age, name, genre, id) values (38, "Alicia Tom", "Anthology", 3);
+insert into author (age, name, genre, id) values (56, "Katy Loin", "Anthology", 4);
+insert into author (age, name, genre, id) values (38, "Martin Leon", "Anthology", 5);
+insert into author (age, name, genre, id) values (56, "Qart Pinkil", "Anthology", 6);
+
+insert into book (isbn, title, id) values ("001-all", "Encyclopedia", 1);
+
+insert into author_book (author_id, book_id) values (1, 1);
+insert into author_book (author_id, book_id) values (2, 1);
+insert into author_book (author_id, book_id) values (3, 1);
+insert into author_book (author_id, book_id) values (4, 1);
+insert into author_book (author_id, book_id) values (5, 1);
+insert into author_book (author_id, book_id) values (6, 1);
