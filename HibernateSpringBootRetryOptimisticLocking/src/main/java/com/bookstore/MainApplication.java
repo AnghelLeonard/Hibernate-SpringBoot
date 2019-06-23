79d94363cf93ab584be4c0a88360a4db75209ba7
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-23 09:36:15 +0300

    Retry optimistic locking

--- HibernateSpringBootRetryOptimisticLocking/src/main/java/com/bookstore/MainApplication.java
+++ HibernateSpringBootRetryOptimisticLocking/src/main/java/com/bookstore/MainApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@Configuration
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class MainApplication {
