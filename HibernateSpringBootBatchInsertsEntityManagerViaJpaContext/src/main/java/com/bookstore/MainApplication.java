cb9ae16cf93faf20d6a71735d3f265f24167a950
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-28 07:51:27 +0300

    Batch inserts via JpaContext

---
+++ HibernateSpringBootBatchInsertsEntityManagerViaJpaContext/src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,28 @@
+package com.bookstore;
+
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final BookstoreService authorService;
+
+    public MainApplication(BookstoreService authorService) {
+        this.authorService = authorService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+            authorService.batchAuthors();
+        };
+    }
+}
---
+++ HibernateSpringBootBatchInsertsEntityManagerViaJpaContext/src/main/java/com/bookstore/dao/Dao.java
@@ -0,0 +1,78 @@
+package com.bookstore.dao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.JpaContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class Dao<T, ID extends Serializable> implements GenericDao<T, ID> {
+
+    private static final Logger logger = Logger.getLogger(Dao.class.getName());
+
+    @Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
+    private int batchSize;
+
+    private final JpaContext jpaContext;
+
+    public Dao(JpaContext jpaContext) {
+        this.jpaContext = jpaContext;
+    }
+
+    @Override
+    public <S extends T> S persist(S entity) {
+        getEntityManager(entity).persist(entity);
+
+        return entity;
+    }
+
+    @Override
+    public <S extends T> Iterable<S> saveInBatch(Iterable<S> entities) {
+
+        if (entities == null) {
+            throw new IllegalArgumentException("The given Iterable of entities not be null!");
+        }
+
+        int i = 0;
+
+        List<S> result = new ArrayList<>();
+
+        for (S entity : entities) {
+            result.add(persist(entity));
+
+            i++;
+
+            // Flush a batch of inserts and release memory
+            if (i % batchSize == 0 && i > 0) {
+                logger.log(Level.INFO,
+                        "Flushing the EntityManager containing {0} entities ...", i);
+
+                getEntityManager(entity).flush();
+                getEntityManager(entity).clear();
+                i = 0;
+            }            
+        }
+        
+        if (i > 0) {
+                logger.log(Level.INFO,
+                        "Flushing the remaining {0} entities ...", i);
+
+                getEntityManager(entities.iterator().next()).flush();
+                getEntityManager(entities.iterator().next()).clear();
+            }
+
+        return result;
+    }
+
+    protected EntityManager getEntityManager(T entity) {
+        return jpaContext.getEntityManagerByManagedType(entity.getClass());
+    }
+}
