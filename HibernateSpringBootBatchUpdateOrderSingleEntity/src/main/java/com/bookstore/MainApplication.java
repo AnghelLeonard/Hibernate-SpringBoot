d2fb42e5a384a2df239a2054800ca72d70fa3ee8
AnghelLeonard <leoprivacy@yahoo.com>
2019-06-29 09:57:45 +0300

    Batch updates (including versioned entities)

--- HibernateSpringBootBatchUpdateOrderSingleEntity/src/main/java/com/bookstore/MainApplication.java
+++ HibernateSpringBootBatchUpdateOrderSingleEntity/src/main/java/com/bookstore/MainApplication.java
@@ -1,7 +1,6 @@
 package com.bookstore;
 
 import com.bookstore.service.BookstoreService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
