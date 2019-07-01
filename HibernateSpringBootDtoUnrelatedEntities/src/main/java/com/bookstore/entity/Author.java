491b045a641d67118868b9817d16c1ce98680665
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-01 11:25:32 +0300

    Dto unrelated entities

--- HibernateSpringBootDtoUnrelatedEntities/src/main/java/com/bookstore/entity/Author.java
+++ HibernateSpringBootDtoUnrelatedEntities/src/main/java/com/bookstore/entity/Author.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity 
 public class Author implements Serializable {
 
     private static final long serialVersionUID = 1L;
