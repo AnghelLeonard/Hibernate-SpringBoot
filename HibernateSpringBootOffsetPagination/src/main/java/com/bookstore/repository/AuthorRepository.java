2fc89ee13f02a89e7b60b6b128f5655c63d16f72
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-04 11:39:42 +0300

    Offset classical pagination

--- HibernateSpringBootOffsetPagination/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootOffsetPagination/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,9 +1,9 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
 }
