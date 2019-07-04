e2bb06baf6a46bbcfe4e3fc8f217dcb8faec8230
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-04 08:02:09 +0300

    Offset clasical pagination

---
+++ HibernateSpringBootOffsetPagination/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,22 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookstoreService {
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Page<Author> fetchNextPage(int page, int size) {
+
+        return authorRepository.findAll(PageRequest.of(page, size));
+    }
+}
--- HibernateSpringBootOffsetPagination/src/main/java/com/jpa/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jpa.repository;
-
-import com.jpa.domain.Player;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends PagingAndSortingRepository<Player, Long> {                
-}
