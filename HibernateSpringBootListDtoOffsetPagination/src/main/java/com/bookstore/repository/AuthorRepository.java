33f26ef190a9e06dab7c3be81d5ce7870443e2ac
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-04 15:21:12 +0300

    Offset pagination via List<Dto>

---
+++ HibernateSpringBootListDtoOffsetPagination/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,18 @@
+package com.bookstore.repository;
+
+import com.bookstore.dto.AuthorDto;
+import com.bookstore.entity.Author;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+
+    @Query(value = "SELECT t.total, name, age FROM author, "
+            + "(SELECT count(*) as total FROM author) AS t "
+            + "ORDER BY age LIMIT ?1, ?2",
+            nativeQuery = true)
+    List<AuthorDto> fetchAll(int page, int size);
+}
