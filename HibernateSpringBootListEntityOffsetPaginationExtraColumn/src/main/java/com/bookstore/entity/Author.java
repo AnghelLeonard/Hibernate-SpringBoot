a0e945ab78882b8b5d42dcf705e45f4b63e2b961
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-06 12:13:37 +0300

    SELECT COUNT subquery + List<Entity> + extra column

--- HibernateSpringBootListEntityOffsetPaginationExtraColumn/src/main/java/com/bookstore/entity/Author.java
+++ HibernateSpringBootListEntityOffsetPaginationExtraColumn/src/main/java/com/bookstore/entity/Author.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+ 
 @Entity
 public class Author implements Serializable {
 
