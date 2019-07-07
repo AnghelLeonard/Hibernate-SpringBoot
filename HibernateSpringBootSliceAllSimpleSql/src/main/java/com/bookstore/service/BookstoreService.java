0d092beaae5b0797d80a29e2c3f5b30067cf9cee
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-07 14:15:21 +0300

    Slice<T> findAll() simple

---
+++ HibernateSpringBootSliceAllSimpleSql/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,22 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookstoreService {
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Slice<Author> fetchNextSlice(int page, int size) {
+
+        return authorRepository.findAll(PageRequest.of(page, size));
+    }
+}
