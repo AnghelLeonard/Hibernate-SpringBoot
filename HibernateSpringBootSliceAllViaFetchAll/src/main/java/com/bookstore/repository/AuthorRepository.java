f420c5924db77a0808f2b0c4eebf2bcb8c9e51ca
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-08 09:46:08 +0300

    Slice<entity> and Slice<dto> via fetchAll()

--- HibernateSpringBootSliceAllViaFetchAll/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootSliceAllViaFetchAll/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -4,13 +4,13 @@ import com.bookstore.dto.AuthorDto;
 import com.bookstore.entity.Author;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
 
     @Transactional(readOnly = true)
     @Query(value = "SELECT a FROM Author a")
