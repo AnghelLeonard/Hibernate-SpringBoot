aa9a65afaeff902e370ff0d04e46d55e546d4b6f
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-17 15:46:07 +0300

    Configure C3P0 via properties and DataSourceBuilder

--- HibernateSpringBootDataSourceBuilderC3P0Kickoff/src/main/java/com/app/MainApplication.java
+++ HibernateSpringBootDataSourceBuilderC3P0Kickoff/src/main/java/com/app/MainApplication.java
@@ -1,33 +1,33 @@
-package com.jpa;
+package com.app;
 
-import java.util.Random;
+import com.app.repository.NumberRepository;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class C3P0Application {
+public class MainApplication {
 
-    private static final Logger logger = Logger.getLogger(C3P0Application.class.getName());   
-    
+    private static final Logger logger = Logger.getLogger(MainApplication.class.getName());
+        
     private static final ExecutorService executor = Executors.newFixedThreadPool(25);
 
-    @Autowired
-    private ApplicationContext applicationContext;    
-    
-    @Autowired
-    private DataSource dataSource;
+    private final NumberRepository numberRepository;    
+    private final DataSource dataSource;
+
+    public MainApplication(NumberRepository numberRepository, DataSource dataSource) {
+        this.numberRepository = numberRepository;
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(C3P0Application.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 
     @Bean
@@ -38,12 +38,10 @@ public class C3P0Application {
             logger.log(Level.INFO, "DataSource: {0}", dataSource);
             logger.info("-------------------------------------------------------");
             
-            while(true) {
-                SampleRepository sampleThread
-                        = applicationContext.getBean(SampleRepository.class);
-                executor.execute(sampleThread);
-                
-                Thread.sleep(new Random().nextInt(125));
+            while (true) {
+                executor.execute(numberRepository);
+
+                Thread.sleep((int) (Math.random() * 250));
             }
 
         };
--- HibernateSpringBootDataSourceBuilderC3P0Kickoff/src/main/resources/data-mysql.sql
+++
@@ -1,7 +0,0 @@
--- Drop
-DROP TABLE IF EXISTS `samples_db`.`samples`;
-
--- Create the table 
-CREATE TABLE `samples_db`.`samples` (
-  `sample` INT DEFAULT NULL
-);
