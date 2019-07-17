850ddfabc84217eb9246664d4edeca11e146c450
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-17 08:04:09 +0300

    Avoid lazy initialization in OSIV

--- HibernateSpringBootSuppressLazyInitInOpenSessionInView/src/main/java/com/bookstore/controller/BookstoreController.java
+++ HibernateSpringBootSuppressLazyInitInOpenSessionInView/src/main/java/com/bookstore/controller/BookstoreController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthorController {
+public class BookstoreController {
 
     private final BookstoreService bookstoreService;
 
-    public AuthorController(BookstoreService bookstoreService) {
+    public BookstoreController(BookstoreService bookstoreService) {
         this.bookstoreService = bookstoreService;
     }
 
