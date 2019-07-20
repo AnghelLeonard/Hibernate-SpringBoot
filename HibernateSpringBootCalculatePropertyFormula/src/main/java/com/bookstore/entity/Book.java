d6d2f9eee30ffc252011fbc8ea0b75da6a61c76b
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-20 16:14:14 +0300

    Calculate property via Formula

--- HibernateSpringBootCalculatePropertyFormula/src/main/java/com/bookstore/entity/Book.java
+++ HibernateSpringBootCalculatePropertyFormula/src/main/java/com/bookstore/entity/Book.java
@@ -12,7 +12,7 @@ import org.hibernate.annotations.Formula;
 public class Book implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+ 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
