883819cfc67fdc0d82565633ee2517dd16ac53ee
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-22 07:43:48 +0300

    Calculate property via Formula

--- HibernateSpringBootCalculatePropertyFormula/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootCalculatePropertyFormula/src/main/java/com/bookstore/service/BookstoreService.java
@@ -1,17 +1,13 @@
 package com.bookstore.service;
 
 import com.bookstore.repository.BookRepository;
-import java.util.logging.Logger;
 import com.bookstore.entity.Book;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookstoreService {
-    
-    private static final Logger logger
-            = Logger.getLogger(BookstoreService.class.getName());
-    
+         
     private final BookRepository bookRepository;
     
     public BookstoreService(BookRepository bookRepository) {        
