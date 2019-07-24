e1ede050d6575574d05fa7af108c98ec2beac2ef
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-24 10:38:45 +0300

    Add created, createdBy, lastModified, lastModifiedBy

--- HibernateSpringBootTimestampGeneration/src/main/java/com/bookstore/service/UserService.java
+++ HibernateSpringBootTimestampGeneration/src/main/java/com/bookstore/service/UserService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public String getCurrentUserName() {
-        // use Spring Security to retrive the current user
+        // use Spring Security to retrive the currently logged-in user(s)
         return Arrays.asList("mark1990", "adrianm", "dan555")
                 .get(new Random().nextInt(3));
     }
