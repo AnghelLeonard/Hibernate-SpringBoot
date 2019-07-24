d2c978f5cd7346d25e1fb67c4a5c7241f7b981be
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-24 13:47:53 +0300

    Hibernate Envers Auditing via schema-sql

---
+++ HibernateSpringBootEnversSchemaSql/README.md
@@ -0,0 +1,17 @@
+**[Hibernate Envers Auditing](https://github.com/AnghelLeonard/Hibernate-SpringBoot/tree/master/HibernateSpringBootEnvers)**
+ 
+**Description:** Auditing is useful for maintaining history records. This can later help us in tracking user activities. 
+ 
+**Key points:**\
+     - in `pom.xml` add the dependency `hibernate-envers`\
+     - each entity that should be audited should be annotated with `@Audited`\
+     - optionally, annotate entities with `@AuditTable` to rename the table used for auditing\
+     - rely on `ValidityAuditStrategy`
+     
+-------------------------------
+
+**You may like to try as well:**
+<a href="https://leanpub.com/java-persistence-performance-illustrated-guide"><p align="center"><img src="https://github.com/AnghelLeonard/Hibernate-SpringBoot/blob/master/Java%20Persistence%20Performance%20Illustrated%20Guide.jpg" height="410" width="350"/></p></a>
+
+
+
---
+++ HibernateSpringBootEnversSchemaSql/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,98 @@
+package com.bookstore.service;
+
+import com.bookstore.repository.BookRepository;
+import com.bookstore.repository.AuthorRepository;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import com.bookstore.entity.Author;
+import com.bookstore.entity.Book;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.hibernate.envers.query.AuditQuery;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookstoreService {
+
+    private static final Logger logger
+            = Logger.getLogger(BookstoreService.class.getName());
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final EntityManager em;
+
+    public BookstoreService(AuthorRepository authorRepository,
+            BookRepository bookRepository, EntityManager em) {
+
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.em = em;
+    }
+
+    @Transactional
+    public void registerAuthor() {
+
+        Author a1 = new Author();
+        a1.setName("Quartis Young");
+        a1.setGenre("Anthology");
+        a1.setAge(34);
+
+        Author a2 = new Author();
+        a2.setName("Mark Janel");
+        a2.setGenre("Anthology");
+        a2.setAge(23);
+
+        Book b1 = new Book();
+        b1.setIsbn("001");
+        b1.setTitle("The Beatles Anthology");
+
+        Book b2 = new Book();
+        b2.setIsbn("002");
+        b2.setTitle("A People's Anthology");
+
+        Book b3 = new Book();
+        b3.setIsbn("003");
+        b3.setTitle("Anthology Myths");
+
+        a1.addBook(b1);
+        a1.addBook(b2);
+        a2.addBook(b3);
+
+        authorRepository.save(a1);
+        authorRepository.save(a2);
+    }
+
+    @Transactional
+    public void updateAuthor() {
+        Author author = authorRepository.findByName("Mark Janel");
+
+        author.setAge(45);
+    }
+
+    @Transactional
+    public void updateBooks() {
+        Author author = authorRepository.findByName("Quartis Young");
+        List<Book> books = author.getBooks();
+
+        for (Book book : books) {
+            book.setIsbn("not available");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void queryEntityHistory() {
+        AuditReader reader = AuditReaderFactory.get(em.unwrap(Session.class));
+
+        AuditQuery queryAtRev = reader.createQuery().forEntitiesAtRevision(Book.class, 3);
+        System.out.println("Get all Book instances modified at revision #3:");
+        System.out.println(queryAtRev.getResultList());
+
+        AuditQuery queryOfRevs = reader.createQuery().forRevisionsOfEntity(Book.class, true, true);
+        System.out.println("\nGet all Book instances in all their states that were audited:");
+        System.out.println(queryOfRevs.getResultList());
+
+    }
+}
