fd15a28614fbdcdd349360d5b112a108d45df323
AnghelLeonard <leoprivacy@yahoo.com>
2019-07-30 11:24:36 +0300

    NamedEntityGraph

---
+++ HibernateSpringBootNamedEntityGraph/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,106 @@
+package com.bookstore.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import javax.persistence.OneToMany;
+
+@Entity
+@NamedEntityGraph(
+  name = "author-books-graph",
+  attributeNodes = {  
+    @NamedAttributeNode("books")
+  }
+)
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String genre;
+    private int age;
+
+    @OneToMany(cascade = CascadeType.ALL,
+            mappedBy = "author", orphanRemoval = true)    
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        this.books.add(book);
+        book.setAuthor(this);
+    }
+
+    public void removeBook(Book book) {
+        book.setAuthor(null);
+        this.books.remove(book);
+    }
+
+    public void removeBooks() {
+        Iterator<Book> iterator = this.books.iterator();
+
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+
+            book.setAuthor(null);
+            iterator.remove();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + id + ", name=" + name
+                + ", genre=" + genre + ", age=" + age + '}';
+    }
+
+}
