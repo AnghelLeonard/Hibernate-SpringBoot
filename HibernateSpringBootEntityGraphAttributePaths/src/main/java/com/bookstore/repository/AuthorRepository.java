9fcb8e89fc8a3d78870472f371f5a099c7ff8276
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-01 09:23:40 +0300

    Entity graph and attributePaths

--- HibernateSpringBootEntityGraphAttributePaths/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootEntityGraphAttributePaths/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    @Transactional(readOnly = true)
+    
     @EntityGraph(attributePaths = {"books"}, 
             type = EntityGraph.EntityGraphType.FETCH)
     @Override
