489c5c6ab8a7c7414bb8ae469debacff2dfa62a9
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-06 09:36:34 +0300

    Using schema-*.sql

---
+++ HibernateSpringBootSchemaSql/src/main/resources/schema-mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS author (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  age int(11) NOT NULL,
+  genre varchar(255) DEFAULT NULL,
+  name varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
