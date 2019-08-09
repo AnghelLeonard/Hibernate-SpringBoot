0232b935b4517af6872a3a1cfd5785377dc95ffa
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-09 10:26:56 +0300

    Table-per-class inheritance

--- HibernateSpringBootTablePerTableInheritance/src/main/java/com/bookstore/entity/Book.java
+++ HibernateSpringBootTablePerTableInheritance/src/main/java/com/bookstore/entity/Book.java
@@ -1,7 +1,6 @@
 package com.bookstore.entity;
 
 import java.io.Serializable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
