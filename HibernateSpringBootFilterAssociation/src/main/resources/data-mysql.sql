a6fbdcf63b47f65b8296a851e158dbfcf34bedbb
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-13 09:15:24 +0300

    Filter association via @Where

--- HibernateSpringBootFilterAssociation/src/main/resources/data-mysql.sql
+++ HibernateSpringBootFilterAssociation/src/main/resources/data-mysql.sql
@@ -2,4 +2,4 @@ insert into author (age, name, genre, id) values (34, "Joana Nimar", "History", 
 insert into book (isbn, title, price, author_id, id) values ("001-JN", "A History of Ancient Prague", 25, 1, 1);
 insert into book (isbn, title, price, author_id, id) values ("002-JN", "A People's History", 15, 1, 2);
 insert into book (isbn, title, price, author_id, id) values ("003-JN", "Ancients", 19, 1, 3);
-insert into book (isbn, title, price, author_id, id) values ("003-JN", "History of the World", 24, 1, 4);
+insert into book (isbn, title, price, author_id, id) values ("004-JN", "History of the World", 24, 1, 4);
