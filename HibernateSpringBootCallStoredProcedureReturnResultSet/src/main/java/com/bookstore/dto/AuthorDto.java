f68754845cfe7392173242156b0f8ce39d90654c
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-06 14:54:40 +0300

    Call stored procedure with result set (entity and DTO)

--- HibernateSpringBootCallStoredProcedureReturnResultSet/src/main/java/com/bookstore/dto/AuthorDto.java
+++ HibernateSpringBootCallStoredProcedureReturnResultSet/src/main/java/com/bookstore/dto/AuthorDto.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class AuthorDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
     private final String nickname;
     private final int age;
 
