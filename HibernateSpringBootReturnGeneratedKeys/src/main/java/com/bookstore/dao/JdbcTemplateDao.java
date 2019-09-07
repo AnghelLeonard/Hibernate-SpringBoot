f01539b17b38b06c1b3bd95128da16c74681888a
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-07 11:02:27 +0300

    How to obtain auto-generated keys

---
+++ HibernateSpringBootReturnGeneratedKeys/src/main/java/com/bookstore/dao/JdbcTemplateDao.java
@@ -0,0 +1,47 @@
+package com.bookstore.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcTemplateDao {
+
+    private static final String SQL_INSERT
+            = "INSERT INTO author (age, name, genre) VALUES (?, ?, ?);";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public long insertAuthor(int age, String name, String genre) {
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+            
+            // or
+            // PreparedStatement ps = connection
+            //      .prepareStatement(SQL_INSERT, new String[]{"id"});
+            
+            // or
+            // PreparedStatement ps = connection
+            //      .prepareStatement(SQL_INSERT, new int[] {1});
+            
+            ps.setInt(1, age);
+            ps.setString(2, name);
+            ps.setString(3, genre);
+            
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+}
---
+++ HibernateSpringBootReturnGeneratedKeys/src/main/java/com/bookstore/dao/SimpleJdbcInsertDao.java
@@ -0,0 +1,22 @@
+package com.bookstore.dao;
+
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SimpleJdbcInsertDao {
+
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public SimpleJdbcInsertDao(DataSource dataSource) {
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("author").usingGeneratedKeyColumns("id");
+    }
+
+    public long insertAuthor(int age, String name, String genre) {
+        return simpleJdbcInsert.executeAndReturnKey(
+                Map.of("age", age, "name", name, "genre", genre)).longValue();
+    }
+}
