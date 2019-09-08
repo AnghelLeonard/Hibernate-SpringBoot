fc3e76eb91d287ffa48fbe346d3c9b77b3633aac
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-08 07:56:31 +0300

    Unproxy a Proxy

--- HibernateSpringBootUnproxyAProxy/src/main/java/com/bookstore/MainApplication.java
+++ HibernateSpringBootUnproxyAProxy/src/main/java/com/bookstore/MainApplication.java
@@ -1,7 +1,6 @@
 package com.bookstore;
 
 import com.bookstore.service.BookstoreService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +8,13 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MainApplication {
+   
+    private final BookstoreService bookstoreService;
 
-    @Autowired
-    private BookstoreService bookstoreService;
-
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+        
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
