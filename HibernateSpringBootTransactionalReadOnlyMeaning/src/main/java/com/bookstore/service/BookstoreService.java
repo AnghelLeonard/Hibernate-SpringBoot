bcdf6da2d52bfd2f469f10f76fc01eea9025a33f
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-18 09:10:35 +0300

    What is Transactional(readOnly=true)

---
+++ HibernateSpringBootTransactionalReadOnlyMeaning/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,107 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.hibernate.engine.spi.EntityEntry;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.engine.spi.Status;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookstoreService {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository, EntityManager entityManager) {
+        this.authorRepository = authorRepository;
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public void readWriteMode() {
+        Author author = authorRepository.findById(1L).orElseThrow();
+
+        org.hibernate.engine.spi.PersistenceContext persistenceContext = getPersistenceContext();
+
+        EntityEntry entityEntryAfterFetch = persistenceContext.getEntry(author);
+        Object[] loadedStateAfterFetch = entityEntryAfterFetch.getLoadedState();
+        Status statusAfterFetch = entityEntryAfterFetch.getStatus();
+
+        displayInformation("After fetch", author, statusAfterFetch, loadedStateAfterFetch);
+        System.out.println("Entity entry : " + entityEntryAfterFetch);
+
+        author.setAge(40);
+        author.setGenre("Horror");
+
+        EntityEntry entityEntryAfterUpdate = persistenceContext.getEntry(author);
+        Object[] loadedStateAfterUpdate = entityEntryAfterUpdate.getLoadedState();
+        Status statusAfterUpdate = entityEntryAfterUpdate.getStatus();
+
+        displayInformation("After update", author, statusAfterUpdate, loadedStateAfterUpdate);
+        System.out.println("Entity entry : " + entityEntryAfterUpdate);
+
+        authorRepository.flush();
+
+        EntityEntry entityEntryAfterFlush = persistenceContext.getEntry(author);
+        Object[] loadedStateAfterFlush = entityEntryAfterFlush.getLoadedState();
+        Status statusAfterFlush = entityEntryAfterFlush.getStatus();
+
+        displayInformation("After flush", author, statusAfterFlush, loadedStateAfterFlush);
+        System.out.println("Entity entry : " + entityEntryAfterFlush);
+    }
+
+    @Transactional(readOnly = true)
+    public void readOnlyMode() {
+        Author author = authorRepository.findById(1L).orElseThrow();
+
+        org.hibernate.engine.spi.PersistenceContext persistenceContext = getPersistenceContext();
+
+        EntityEntry entityEntryAfterFetch = persistenceContext.getEntry(author);
+        Object[] loadedStateAfterFetch = entityEntryAfterFetch.getLoadedState();
+        Status statusAfterFetch = entityEntryAfterFetch.getStatus();
+        
+        displayInformation("After fetch", author, statusAfterFetch, loadedStateAfterFetch);
+        System.out.println("Entity entry : " + entityEntryAfterFetch);
+
+        author.setAge(40);
+        author.setGenre("Horror");
+
+        EntityEntry entityEntryAfterUpdate = persistenceContext.getEntry(author);        
+        Object[] loadedStateAfterUpdate = entityEntryAfterUpdate.getLoadedState();
+        Status statusAfterUpdate = entityEntryAfterUpdate.getStatus();
+
+        displayInformation("After update", author, statusAfterUpdate, loadedStateAfterUpdate);
+        System.out.println("Entity entry : " + entityEntryAfterUpdate);
+
+        authorRepository.flush();
+
+        EntityEntry entityEntryAfterFlush = persistenceContext.getEntry(author);
+        Object[] loadedStateAfterFlush = entityEntryAfterFlush.getLoadedState();
+        Status statusAfterFlush = entityEntryAfterFlush.getStatus();
+
+        displayInformation("After flush", author, statusAfterFlush, loadedStateAfterFlush);
+        System.out.println("Entity entry : " + entityEntryAfterFlush);
+    }
+
+    private org.hibernate.engine.spi.PersistenceContext getPersistenceContext() {
+        SharedSessionContractImplementor sharedSession = entityManager.unwrap(
+                SharedSessionContractImplementor.class
+        );
+
+        return sharedSession.getPersistenceContext();
+    }
+
+    private void displayInformation(String phase, Author author, Status status, Object[] loadedState) {
+        System.out.println("\n-------------------------------------");
+        System.out.println("Phase:" + phase);
+        System.out.println("Entity: " + author);        
+        System.out.println("Status:" + status);
+        System.out.println("Loaded state: " + Arrays.toString(loadedState));
+    }
+}
