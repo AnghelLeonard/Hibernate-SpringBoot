239074d491c106e3ff3892a863a7502e7045daf8
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-19 14:28:51 +0300

    Best one-to-many mapping

--- HibernateSpringBootOneToManyBidirectional/src/main/java/com/bookstore/repository/BookRepository.java
+++ HibernateSpringBootOneToManyBidirectional/src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,6 +1,5 @@
 package com.bookstore.repository;
 
-import com.bookstore.entity.Author;
 import com.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
