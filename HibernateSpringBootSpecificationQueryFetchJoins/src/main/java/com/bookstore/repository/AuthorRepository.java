c9a63cf74a0de2e038ce2f7f0c87e347e77f1ba8
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-28 18:56:36 +0300

    Creating Specification query fetch joins

--- HibernateSpringBootSpecificationQueryFetchJoins/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootSpecificationQueryFetchJoins/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,19 +1,27 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.Author;
+import javax.persistence.QueryHint;
+import static org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long>,
         JpaSpecificationExecutor<Author> {
-    
+
     @Transactional(readOnly = true)
     @Query(value = "SELECT a.id FROM Author a WHERE a.genre = ?1")
     Page<Long> fetchPageOfIdsByGenre(String genre, Pageable pageable);
+
+    @Override
+    @QueryHints(value = @QueryHint(name = HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    public Page<Author> findAll(Specification<Author> spec, Pageable pageable);
 }
