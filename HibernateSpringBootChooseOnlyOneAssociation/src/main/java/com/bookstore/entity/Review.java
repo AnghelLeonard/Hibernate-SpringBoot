54e36b6f17c3ee503d5f9d77486d968c6ccbf83b
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-29 13:36:45 +0300

    Just one non-null association

--- HibernateSpringBootChooseOnlyOneAssociation/src/main/java/com/bookstore/entity/Review.java
+++ HibernateSpringBootChooseOnlyOneAssociation/src/main/java/com/bookstore/entity/Review.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
-@JustOneOfMany
+@JustOneOfMany // for testing the TRIGGER from data-mysql.sql you have to comment this line
 public class Review implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- HibernateSpringBootChooseOnlyOneAssociation/src/main/resources/data-mysql.sql
+++ HibernateSpringBootChooseOnlyOneAssociation/src/main/resources/data-mysql.sql
@@ -1,3 +1,17 @@
-insert into article (title, id) values ("CSS3 Tricks", 1);
-insert into magazine (title, id) values ("Java QA", 1);
-insert into book (title, id) values ("Java Coding Problems", 1);
+INSERT INTO article (title, id) VALUES ("CSS3 Tricks", 1) ^;
+INSERT INTO magazine (title, id) VALUES ("Java QA", 1) ^;
+INSERT INTO book (title, id) VALUES ("Java Coding Problems", 1) ^;
+
+DROP TRIGGER IF EXISTS Just_One_Of_Many ^; 
+
+CREATE TRIGGER Just_One_Of_Many
+    BEFORE INSERT ON review
+        FOR EACH ROW 
+            BEGIN 
+                IF (NEW.article_id IS NOT NULL AND NEW.magazine_id IS NOT NULL) OR 
+                   (NEW.article_id IS NOT NULL AND NEW.book_id IS NOT NULL) OR
+                   (NEW.book_id IS NOT NULL AND NEW.magazine_id IS NOT NULL) THEN                    
+                        SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT='A review can be associated with either a book, a magazine or an article';                    
+                END IF;    
+            END ^;
