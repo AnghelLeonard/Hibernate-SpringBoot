56a9d858c2652e659bafa70372f5643cc21995fd
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-29 18:39:35 +0300

    EnumType.ORDINAL and EnumType.STRING

---
+++ HibernateSpringBootEnumStringInt/src/main/java/com/bookstore/enums/RatingType.java
@@ -0,0 +1,5 @@
+package com.bookstore.enums;
+
+public enum RatingType {
+    HATED_IT, DID_NOT_LIKE_IT, LIKED_IT, REALLY_LIKED_IT, LOVE_IT
+}
---
+++ HibernateSpringBootEnumStringInt/src/main/resources/data-mysql.sql
@@ -0,0 +1,6 @@
+insert into author (age, name, genre, rating, id) values (23, "Mark Janel", "ANTHOLOGY", 3, 1);
+insert into author (age, name, genre, rating, id) values (43, "Olivia Goy", "HORROR", 4, 2);
+insert into author (age, name, genre, rating, id) values (51, "Quartis Young", "ANTHOLOGY", 4, 3);
+insert into author (age, name, genre, rating, id) values (34, "Joana Nimar", "HISTORY", 3, 4);
+insert into author (age, name, genre, rating, id) values (38, "Alicia Tom", "ANTHOLOGY", 4, 5);
+insert into author (age, name, genre, rating, id) values (56, "Katy Loin", "ANTHOLOGY", 3, 6);
