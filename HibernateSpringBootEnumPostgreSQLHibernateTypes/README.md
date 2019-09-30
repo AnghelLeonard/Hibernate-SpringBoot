efb5c99c25fc6112bd72a00c1f26968ad3a5a95b
AnghelLeonard <leoprivacy@yahoo.com>
2019-09-30 16:47:39 +0300

    Mapping PostgreSQL enum via Hibernate Types Library

---
+++ HibernateSpringBootEnumPostgreSQLHibernateTypes/README.md
@@ -0,0 +1,10 @@
+**[How To Map Java `enum` To PostgreSQL `enum` Type](https://github.com/AnghelLeonard/Hibernate-SpringBoot/tree/master/HibernateSpringBootEnumPostgreSQLCustomType)**
+ 
+**Description:** This application maps a Java `enum` type to PostgreSQL `enum` type.
+
+**Key points:**\
+     - define a custom Hibernate `EnumType`\
+     - register this custom `EnumType` via `package-info.java`\
+     - annotate the corresponding entity field `@Type`
+     
+<a href="https://leanpub.com/java-persistence-performance-illustrated-guide"><p align="center"><img src="https://github.com/AnghelLeonard/Hibernate-SpringBoot/blob/master/Java%20Persistence%20Performance%20Illustrated%20Guide.jpg" height="410" width="350"/></p></a>
---
+++ HibernateSpringBootEnumPostgreSQLHibernateTypes/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,74 @@
+package com.bookstore.entity;
+
+import com.bookstore.enums.GenreType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+@Entity
+@TypeDef(
+    name = "genre_enum_type",
+    typeClass = PostgreSQLEnumType.class
+)
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int age;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Type(type = "genre_enum_type")
+    @Column(columnDefinition = "genre_info")
+    private GenreType genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GenreType getGenre() {
+        return genre;
+    }
+
+    public void setGenre(GenreType genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + id + ", age=" + age
+                + ", name=" + name + ", genre=" + genre + '}';
+    }
+}
