8812f9bd8e7d8d09f44be59199435d44205ea7ae
AnghelLeonard <leoprivacy@yahoo.com>
2019-10-10 11:06:50 +0300

    Composite PK via Embeddable

--- HibernateSpringBootCompositeKeyEmbeddable/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootCompositeKeyEmbeddable/src/main/java/com/bookstore/service/BookstoreService.java
@@ -1,6 +1,5 @@
 package com.bookstore.service;
 
-import com.bookstore.repository.BookRepository;
 import com.bookstore.repository.AuthorRepository;
 import com.bookstore.entity.Author;
 import com.bookstore.entity.AuthorId;
@@ -12,13 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookstoreService {
 
     private final AuthorRepository authorRepository;
-    private final BookRepository bookRepository;
+ 
+    public BookstoreService(AuthorRepository authorRepository) {
 
-    public BookstoreService(AuthorRepository authorRepository,
-            BookRepository bookRepository) {
-
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository; 
     }
 
     @Transactional
