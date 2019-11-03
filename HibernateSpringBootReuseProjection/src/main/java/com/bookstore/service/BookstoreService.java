00ed3f1c3e5b477f310a1a44f9d654813f4cc7e3
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-03 10:48:07 +0200

    Reuse Spring projection

--- HibernateSpringBootReuseProjection/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootReuseProjection/src/main/java/com/bookstore/service/BookstoreService.java
@@ -2,13 +2,7 @@ package com.bookstore.service;
 
 import com.bookstore.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.hibernate.engine.spi.EntityEntry;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.springframework.transaction.annotation.Transactional;
 import com.bookstore.dto.AuthorDto;
 
@@ -17,21 +11,14 @@ public class BookstoreService {
 
     private final AuthorRepository authorRepository;
 
-    @PersistenceContext
-    private final EntityManager entityManager;
-
-    public BookstoreService(AuthorRepository authorRepository,
-            EntityManager entityManager) {
+    public BookstoreService(AuthorRepository authorRepository) {
 
         this.authorRepository = authorRepository;
-        this.entityManager = entityManager;
     }
 
     @Transactional(readOnly = true)
     public List<AuthorDto> fetchAgeNameGenre() {
         List<AuthorDto> authors = authorRepository.fetchAgeNameGenre();
-
-        briefOverviewOfPersistentContextContent();
 
         return authors;
     }
@@ -39,42 +26,8 @@ public class BookstoreService {
     @Transactional(readOnly = true)
     public List<AuthorDto> fetchNameEmail() {
         List<AuthorDto> authors = authorRepository.fetchNameEmail();
-      
-        briefOverviewOfPersistentContextContent();
 
         return authors;
-    }
-
-    private void briefOverviewOfPersistentContextContent() {
-        org.hibernate.engine.spi.PersistenceContext persistenceContext = getPersistenceContext();
-
-        int managedEntities = persistenceContext.getNumberOfManagedEntities();
-
-        System.out.println("\n-----------------------------------");
-        System.out.println("Total number of managed entities: " + managedEntities);
-
-        Map entities = persistenceContext.getEntitiesByKey();
-        entities.forEach((key, value) -> System.out.println(key + ":" + value));
-
-        entities.values().forEach(entry
-                -> {
-            EntityEntry ee = persistenceContext.getEntry(entry);
-            System.out.println(
-                    "Entity name: " + ee.getEntityName()
-                    + " | Status" + ee.getStatus()
-                    + " | State: " + Arrays.toString(ee.getLoadedState()));
-        });
-
-        System.out.println("\n-----------------------------------\n");
-    }
-
-    private org.hibernate.engine.spi.PersistenceContext getPersistenceContext() {
-
-        SharedSessionContractImplementor sharedSession = entityManager.unwrap(
-                SharedSessionContractImplementor.class
-        );
-
-        return sharedSession.getPersistenceContext();
     }
 
 }
