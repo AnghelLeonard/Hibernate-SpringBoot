efaf57b12c12d0f4b27493dbadf8aa1e85e6fbaf
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-07 16:00:03 +0200

    SELECT COUNT subquery + Page<Entity> + Projection

--- HibernateSpringBootPageEntityOffsetPaginationProjection/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootPageEntityOffsetPaginationProjection/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
 
+    @Transactional
     @Query(value = "SELECT a AS author, (SELECT count(a) FROM Author a) AS total FROM Author a")
     List<AuthorInfo> fetchAll(Pageable pageable);        
 }
