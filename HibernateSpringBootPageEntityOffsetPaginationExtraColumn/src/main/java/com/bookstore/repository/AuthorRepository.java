e88494449200ea9787b9948d5c600c5deba041a8
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-07 16:08:12 +0200

    SELECT COUNT subquery + Page<Entity> + extra column

--- HibernateSpringBootPageEntityOffsetPaginationExtraColumn/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootPageEntityOffsetPaginationExtraColumn/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
 
+    @Transactional
     @Query(value = "SELECT t.total, id, name, age, genre FROM author, "
             + "(SELECT count(*) as total FROM author) AS t",            
             nativeQuery = true)
