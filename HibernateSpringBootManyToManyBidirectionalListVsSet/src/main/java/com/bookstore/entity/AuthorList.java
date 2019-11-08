f5a5ac1f12ee359bc609e23f2d7d53e099afc60e
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-08 12:00:10 +0200

    Many-to-many always use Set

--- HibernateSpringBootManyToManyBidirectionalListVsSet/src/main/java/com/bookstore/entity/AuthorList.java
+++ HibernateSpringBootManyToManyBidirectionalListVsSet/src/main/java/com/bookstore/entity/AuthorList.java
@@ -96,6 +96,10 @@ public class AuthorList implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
+        
+        if(obj == null) {
+            return false;
+        }
 
         if (this == obj) {
             return true;
