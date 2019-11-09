fe6583ac9355067b16c23474df544fcb4fe22e9f
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-09 17:49:23 +0200

    Batch inserts (including associations) batch per transaction

--- HibernateSpringBootBatchInsertOrderBatchPerTransaction/src/main/java/com/bookstore/impl/BatchRepositoryImpl.java
+++ HibernateSpringBootBatchInsertOrderBatchPerTransaction/src/main/java/com/bookstore/impl/BatchRepositoryImpl.java
@@ -5,9 +5,10 @@ import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(readOnly = true)
+@Transactional(propagation = Propagation.NEVER)
 public class BatchRepositoryImpl<T, ID extends Serializable>
         extends SimpleJpaRepository<T, ID> implements BatchRepository<T, ID> {
 
