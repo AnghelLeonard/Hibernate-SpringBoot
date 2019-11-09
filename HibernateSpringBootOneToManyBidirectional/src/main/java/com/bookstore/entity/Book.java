b8e2a0db02b9b7c089fa42851e7ddfab501fa78f
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-09 08:53:17 +0200

    Best one-to-many mapping

--- HibernateSpringBootOneToManyBidirectional/src/main/java/com/bookstore/entity/Book.java
+++ HibernateSpringBootOneToManyBidirectional/src/main/java/com/bookstore/entity/Book.java
@@ -58,14 +58,14 @@ public class Book implements Serializable {
     }
 
     @Override
-    public boolean equals(Object obj) {
-     
-        if (obj == null) { 
-            return false;    
-        }
+    public boolean equals(Object obj) {           
         
         if (this == obj) {
             return true;
+        }
+        
+        if (obj == null) {
+            return false;
         }
         
         if (getClass() != obj.getClass()) {
