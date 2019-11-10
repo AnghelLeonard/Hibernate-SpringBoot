c3e9e88637e6b36788c6b616e826a29fa20b92e0
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-10 13:32:06 +0200

    @Subselect

--- HibernateSpringBootSubselect/src/main/java/com/bookstore/repository/AuthorSummaryRepository.java
+++ HibernateSpringBootSubselect/src/main/java/com/bookstore/repository/AuthorSummaryRepository.java
@@ -3,9 +3,7 @@ package com.bookstore.repository;
 import com.bookstore.summary.AuthorSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional(readOnly = true)
 public interface AuthorSummaryRepository extends JpaRepository<AuthorSummary, Long> {
 }
