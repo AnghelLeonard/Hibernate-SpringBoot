64a70c01bce4151b87d966ad29c5ec84fc2653a0
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-12 16:26:10 +0200

    Read-only entities

---
+++ HibernateSpringBootReadOnlyQueries/src/main/java/com/bookstore/MainApplication.java
@@ -0,0 +1,38 @@
+package com.bookstore;
+
+import com.bookstore.entity.Author;
+import com.bookstore.service.BookstoreService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private final BookstoreService bookstoreService;
+
+    public MainApplication(BookstoreService bookstoreService) {
+        this.bookstoreService = bookstoreService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner init() {
+        return args -> {
+
+            System.out.println("Avoid:\n------\n");
+            Author authorRW = bookstoreService.fetchAuthorReadWriteMode();
+            bookstoreService.updateAuthor(authorRW);
+
+            System.out.println("\n\n=============================\n\n");
+            
+            System.out.println("Recommended:\n-----------\n");
+            Author authorRO = bookstoreService.fetchAuthorReadOnlyMode();
+            bookstoreService.updateAuthor(authorRO);
+        };
+    }
+}
---
+++ HibernateSpringBootReadOnlyQueries/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,99 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import java.util.Arrays;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.hibernate.engine.spi.EntityEntry;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookstoreService {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository, EntityManager entityManager) {
+        this.authorRepository = authorRepository;
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public Author fetchAuthorReadWriteMode() {
+        System.out.println("Persistent Context before fetching read-write entity:");
+        briefOverviewOfPersistentContextContent();
+
+        Author author = authorRepository.findById(1L).orElseThrow();
+
+        System.out.println("\n\nPersistent Context after fetching read-write entity:");
+        briefOverviewOfPersistentContextContent();
+
+        return author;
+    }
+
+    @Transactional(readOnly = true)
+    public Author fetchAuthorReadOnlyMode() {
+        System.out.println("Persistent Context before fetching read-only entity:");
+        briefOverviewOfPersistentContextContent();
+
+        Author author = authorRepository.findById(1L).orElseThrow();
+
+        System.out.println("\n\nPersistent Context after fetching read-only entity:");
+        briefOverviewOfPersistentContextContent();
+
+        return author;
+    }
+    
+    @Transactional
+    public void updateAuthor(Author author) {
+        
+        author.setAge(author.getAge() + 1);
+        authorRepository.save(author);
+        
+        System.out.println("\n\nPersistent Context after update the entity:");
+        briefOverviewOfPersistentContextContent();
+    }
+
+    private void briefOverviewOfPersistentContextContent() {
+        org.hibernate.engine.spi.PersistenceContext persistenceContext = getPersistenceContext();
+
+        int managedEntities = persistenceContext.getNumberOfManagedEntities();
+        Map collectionEntries = persistenceContext.getCollectionEntries();
+
+        System.out.println("\n-----------------------------------");
+        System.out.println("Total number of managed entities: " + managedEntities);
+        if (collectionEntries != null) {
+            System.out.println("Total number of collection entries: "
+                    + (managedEntities - collectionEntries.values().size()));
+        }
+
+        Map entities = persistenceContext.getEntitiesByKey();
+        entities.forEach((key, value) -> System.out.println(key + ":" + value));
+
+        entities.values().forEach(entry
+                -> {
+            EntityEntry ee = persistenceContext.getEntry(entry);
+            System.out.println(
+                    "Entity name: " + ee.getEntityName()
+                    + " | Status: " + ee.getStatus()
+                    + " | State: " + Arrays.toString(ee.getLoadedState()));
+        });
+
+        System.out.println("\n-----------------------------------\n");
+    }
+
+    private org.hibernate.engine.spi.PersistenceContext getPersistenceContext() {
+
+        SharedSessionContractImplementor sharedSession = entityManager.unwrap(
+                SharedSessionContractImplementor.class
+        );
+
+        return sharedSession.getPersistenceContext();
+    }
+}
