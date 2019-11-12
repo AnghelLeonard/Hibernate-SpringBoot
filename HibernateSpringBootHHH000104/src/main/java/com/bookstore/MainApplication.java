cd767ef540d0aa18cf32f40fa2c4be675ad3a776
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-12 13:48:57 +0200

    Fixing HHH000104

--- HibernateSpringBootHHH000104/src/main/java/com/bookstore/MainApplication.java
+++ HibernateSpringBootHHH000104/src/main/java/com/bookstore/MainApplication.java
@@ -1,19 +1,12 @@
 package com.bookstore;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-    }
-
-    @Bean
-    public Hibernate5Module hibernate5Module() {
-        return new Hibernate5Module();
-    }
+    }    
 }
