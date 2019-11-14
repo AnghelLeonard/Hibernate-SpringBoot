825a0d73ca166f21a2ba245188b7a4a60816ad07
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-14 08:39:15 +0200

    IN-list padding

--- HibernateSpringBootINListPadding/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootINListPadding/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     
+    @Transactional(readOnly=true)    
     @Query("SELECT a FROM Author a WHERE a.id IN ?1")
     public List<Author> fetchIn(List<Long> ids);
 }
