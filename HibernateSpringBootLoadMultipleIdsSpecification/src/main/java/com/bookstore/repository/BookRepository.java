574a46267c7674a8b8027d8498bd1d7e5804daa2
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-16 17:45:38 +0200

    Fetch by multiple entities by id (JPQL and Specification)

--- HibernateSpringBootLoadMultipleIdsSpecification/src/main/java/com/bookstore/repository/BookRepository.java
+++ HibernateSpringBootLoadMultipleIdsSpecification/src/main/java/com/bookstore/repository/BookRepository.java
@@ -2,7 +2,6 @@ package com.bookstore.repository;
 
 import com.bookstore.entity.Book;
 import java.util.List;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -15,8 +14,5 @@ public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor< Book> {
 
     @Query("SELECT b FROM Book b WHERE b.id IN ?1")
-    List<Book> fetchByMultipleIds(List<Long> ids);
-
-    @Override
-    List<Book> findAll(Specification<Book> spec);
+    List<Book> fetchByMultipleIds(List<Long> ids);  
 }
