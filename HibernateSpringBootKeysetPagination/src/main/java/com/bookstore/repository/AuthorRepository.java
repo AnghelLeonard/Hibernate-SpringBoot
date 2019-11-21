e13536f5d30347c5a3d54e4b87c87da1ea6990e1
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-21 16:53:47 +0200

    Keyset pagination

--- HibernateSpringBootKeysetPagination/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootKeysetPagination/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -6,14 +6,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     @Query(value = "SELECT * FROM author AS a WHERE a.id < ?1 ORDER BY a.id DESC LIMIT ?2",
             nativeQuery = true)
     List<Author> fetchAll(long id, int limit);
-    
+
     @Query(value = "SELECT name, age FROM author AS a WHERE a.id < ?1 ORDER BY a.id DESC LIMIT ?2",
             nativeQuery = true)
     List<AuthorDto> fetchAllDto(long id, int limit);
