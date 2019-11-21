ac89f306e96961ac108e7e9e6d78417c9f362879
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-21 16:50:52 +0200

    Keyset pagination with Next Page button

--- HibernateSpringBootKeysetPaginationNextPage/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootKeysetPaginationNextPage/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly=true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     @Query(value = "SELECT * FROM author AS a WHERE a.id < ?1 ORDER BY a.id DESC LIMIT ?2",
