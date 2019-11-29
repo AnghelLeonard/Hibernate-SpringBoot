4fbe07e9ef1c50c608bd6cc89103a37740ee541f
AnghelLeonard <leoprivacy@yahoo.com>
2019-11-29 17:03:17 +0200

    Pagination via ROW_NUMBER()

--- HibernateSpringBootPaginationRowNumber/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootPaginationRowNumber/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -3,15 +3,15 @@ package com.bookstore.repository;
 import com.bookstore.dto.AuthorDto;
 import com.bookstore.entity.Author;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
-
-    @Transactional(readOnly = true)
+@Transactional(readOnly = true)
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    
     @Query(value = "SELECT * FROM (SELECT name, age, COUNT(*) OVER() AS total, "
             + "ROW_NUMBER() OVER (ORDER BY age) AS row_num FROM author) AS a "
             + "WHERE row_num BETWEEN ?1 AND ?2",
