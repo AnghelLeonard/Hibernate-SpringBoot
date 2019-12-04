13330ae5be8ec14afa878914c9c00477af97ef89
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-04 15:02:15 +0200

    GenerationType.AUTO and UUID

---
+++ HibernateSpringBootAutoUUID/src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,62 @@
+package com.bookstore.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    private int age;
+    private String name;
+    private String genre;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + id + ", age=" + age
+                + ", name=" + name + ", genre=" + genre + '}';
+    }
+}
---
+++ HibernateSpringBootAutoUUID/src/main/java/com/bookstore/service/BookstoreService.java
@@ -0,0 +1,24 @@
+package com.bookstore.service;
+
+import com.bookstore.entity.Author;
+import com.bookstore.repository.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookstoreService {
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public void insertAuthor() {
+        Author author = new Author();
+        author.setName("Joana Nimar");
+        author.setGenre("History");
+        author.setAge(34);
+
+        authorRepository.save(author);
+    }
+}
