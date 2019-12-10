db561e3e8eb9429198f2798594968920e764b932
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-10 10:41:31 +0200

    Hibernate session-level repeatable reads

--- HibernateSpringBootSessionRepeatableReads/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootSessionRepeatableReads/src/main/java/com/bookstore/service/BookstoreService.java
@@ -35,8 +35,8 @@ public class BookstoreService {
             @Override
             protected void doInTransactionWithoutResult(TransactionStatus status) {
                 
-                Author authorA = authorRepository.findById(1L).orElseThrow();
-                System.out.println("Author A: " + authorA.getName() + "\n");
+                Author authorA1 = authorRepository.findById(1L).orElseThrow();
+                System.out.println("Author A1: " + authorA1.getName() + "\n");
                 
                 // Transaction B
                 template.execute(new TransactionCallbackWithoutResult() {
@@ -49,11 +49,18 @@ public class BookstoreService {
                     }
                 });
                 
+                // Direct fetching via findById(), find() and get() doesn't trigger a SELECT
+                // It loads the author directly from Persistence Context
+                Author authorA2 = authorRepository.findById(1L).orElseThrow();
+                System.out.println("\nAuthor A2: " + authorA2.getName() + "\n");
+                
                 // JPQL entity queries take advantage of session-level repeatable reads
+                // The data snapshot returned by the triggered SELECT is ignored
                 Author authorViaJpql = authorRepository.fetchByIdJpql(1L);
                 System.out.println("Author via JPQL: " + authorViaJpql.getName() + "\n");
                 
                 // SQL entity queries take advantage of session-level repeatable reads
+                // The data snapshot returned by the triggered SELECT is ignored
                 Author authorViaSql = authorRepository.fetchByIdSql(1L);
                 System.out.println("Author via SQL: " + authorViaSql.getName() + "\n");
                 
