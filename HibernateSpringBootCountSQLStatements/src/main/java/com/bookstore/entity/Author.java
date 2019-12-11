deac1838933924ec7bd7aa1e01c269d1cf0810f8
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-11 10:37:22 +0200

    Count and assert SQL

--- HibernateSpringBootCountSQLStatements/src/main/java/com/bookstore/entity/Author.java
+++ HibernateSpringBootCountSQLStatements/src/main/java/com/bookstore/entity/Author.java
@@ -1,15 +1,10 @@
 package com.bookstore.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Author implements Serializable {
@@ -23,31 +18,6 @@ public class Author implements Serializable {
     private String name;
     private String genre;
     private int age;
-
-    @OneToMany(cascade = CascadeType.ALL,
-            mappedBy = "author", orphanRemoval = true)
-    private List<Book> books = new ArrayList<>();
-
-    public void addBook(Book book) {
-        this.books.add(book);
-        book.setAuthor(this);
-    }
-
-    public void removeBook(Book book) {
-        book.setAuthor(null);
-        this.books.remove(book);
-    }
-
-    public void removeBooks() {
-        Iterator<Book> iterator = this.books.iterator();
-
-        while (iterator.hasNext()) {
-            Book book = iterator.next();
-
-            book.setAuthor(null);
-            iterator.remove();
-        }
-    }
 
     public Long getId() {
         return id;
@@ -71,7 +41,7 @@ public class Author implements Serializable {
 
     public void setGenre(String genre) {
         this.genre = genre;
-    }  
+    }
 
     public int getAge() {
         return age;
@@ -81,18 +51,10 @@ public class Author implements Serializable {
         this.age = age;
     }
 
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
     @Override
     public String toString() {
-        return "Author{" + "id=" + id + ", name=" + name 
+        return "Author{" + "id=" + id + ", name=" + name
                 + ", genre=" + genre + ", age=" + age + '}';
     }
-    
+
 }
