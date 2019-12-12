b732b978426c82c92053259babf5809e466e06b7
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-12 13:13:38 +0200

    Clone entities

--- HibernateSpringBootCloneEntity/src/main/java/com/bookstore/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.repository;
-
-import com.bookstore.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
