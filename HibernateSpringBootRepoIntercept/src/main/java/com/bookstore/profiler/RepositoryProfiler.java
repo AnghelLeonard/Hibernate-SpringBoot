6d27fe17a9c7e11ec5855956ce8f67bcda9f5c90
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-15 10:39:57 +0200

    Log query-method execution time

--- HibernateSpringBootRepoIntercept/src/main/java/com/bookstore/profiler/RepositoryProfiler.java
+++ HibernateSpringBootRepoIntercept/src/main/java/com/bookstore/profiler/RepositoryProfiler.java
@@ -11,16 +11,18 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class RepositoryProfiler {
+
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Pointcut("execution(public * org.springframework.data.repository.Repository+.*(..))")
-    public void intercept() {}
+    public void intercept() {
+    }
 
     @Around("intercept()")
     public Object profile(ProceedingJoinPoint joinPoint) {
-        
+
         long startMs = System.currentTimeMillis();
-        
+
         Object result = null;
         try {
             result = joinPoint.proceed();
@@ -28,14 +30,14 @@ public class RepositoryProfiler {
             logger.error(e.getMessage(), e);
             // do whatever you want with the exception
         }
-        
+
         long elapsedMs = System.currentTimeMillis() - startMs;
-        
+
         // you may like to use logger.debug
-        logger.info(joinPoint.getTarget()+"."+joinPoint.getSignature()
-                +": Execution time: " + elapsedMs + " ms");
+        logger.info(joinPoint.getTarget() + "." + joinPoint.getSignature()
+                + ": Execution time: " + elapsedMs + " ms");
 
         // pay attention that this line may return null
-        return result; 
+        return result;
     }
 }
