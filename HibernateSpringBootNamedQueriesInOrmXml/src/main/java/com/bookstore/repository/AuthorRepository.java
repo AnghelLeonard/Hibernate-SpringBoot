8c6ff3ed0470131387bf31724ae82cba274d1a19
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-16 18:36:48 +0200

    Named query via orm.xml

--- HibernateSpringBootNamedQueriesInOrmXml/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootNamedQueriesInOrmXml/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -1,25 +1,60 @@
 package com.bookstore.repository;
 
-import com.bookstore.dto.AuthorDto;
 import java.util.List;
 import com.bookstore.entity.Author;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AuthorRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
 
-    List<Author> fetchAllDesc();
-
-    Page<Author> fetchPageDesc(Pageable pageable);
+    List<Author> fetchAll();
 
     Author fetchByNameAndAge(String name, int age);
 
+    // causes exception
+    // List<Author> fetchViaSort(Sort sort);
+    
+    // causes exception
+    // List<Author> fetchViaSortWhere(int age, Sort sort);
+    
+    Page<Author> fetchPageSort(Pageable pageable);
+    
+    Page<Author> fetchPageSortWhere(int age, Pageable pageable);
+    
+    Slice<Author> fetchSliceSort(Pageable pageable);
+    
+    Slice<Author> fetchSliceSortWhere(int age, Pageable pageable);
+    
     @Query(nativeQuery = true)
-    List<AuthorDto> fetchNameAndAge();
+    List<Author> fetchAllNative();
+    
+    @Query(nativeQuery = true)
+    Author fetchByNameAndAgeNative(String name, int age);
+    
+    // causes exception
+    // @Query(nativeQuery = true)
+    // List<Author> fetchViaSortNative(Sort sort);
+
+    // causes exception
+    // @Query(nativeQuery = true)
+    // List<Author> fetchViaSortWhereNative(int age, Sort sort);
+    
+    @Query(nativeQuery = true)
+    Page<Author> fetchPageSortNative(Pageable pageable);
+    
+    @Query(nativeQuery = true)
+    Page<Author> fetchPageSortWhereNative(int age, Pageable pageable);
+    
+    @Query(nativeQuery = true)
+    Slice<Author> fetchSliceSortNative(Pageable pageable);
+    
+    @Query(nativeQuery = true)
+    Slice<Author> fetchSliceSortWhereNative(int age, Pageable pageable);
 }
--- HibernateSpringBootNamedQueriesInOrmXml/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootNamedQueriesInOrmXml/src/main/java/com/bookstore/service/BookstoreService.java
@@ -1,12 +1,13 @@
 package com.bookstore.service;
 
-import com.bookstore.dto.AuthorDto;
 import java.util.List;
 import com.bookstore.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 import com.bookstore.entity.Author;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 
 @Service
 public class BookstoreService {
@@ -17,22 +18,83 @@ public class BookstoreService {
         this.authorRepository = authorRepository;
     }
 
-    public List<Author> fetchAllAuthorsDesc() {
-
-        return authorRepository.fetchAllDesc();
-    }
-
-    public Page<Author> fetchAuthorsPageDesc() {
-
-        return authorRepository.fetchPageDesc(PageRequest.of(0, 2));
+    public List<Author> fetchAllAuthors() {
+        return authorRepository.fetchAll();
     }
 
     public Author fetchAuthorByNameAndAge() {
         return authorRepository.fetchByNameAndAge("Joana Nimar", 34);
     }
 
-    public List<AuthorDto> fetchAuthorsNamesAndAges() {
-
-        return authorRepository.fetchNameAndAge();
+    /* causes exception
+    public List<Author> fetchAuthorsViaSort() {
+        return authorRepository.fetchViaSort(Sort.by(Direction.DESC, "name"));
     }
+    */
+    
+    /* causes exception
+    public List<Author> fetchAuthorsViaSortWhere() {
+        return authorRepository.fetchViaSortWhere(30, Sort.by(Direction.DESC, "name"));
+    }
+    */
+    
+    public Page<Author> fetchAuthorsPageSort() {
+        return authorRepository.fetchPageSort(PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public Page<Author> fetchAuthorsPageSortWhere() {
+        return authorRepository.fetchPageSortWhere(30, PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public Slice<Author> fetchAuthorsSliceSort() {
+        return authorRepository.fetchSliceSort(PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public Slice<Author> fetchAuthorsSliceSortWhere() {
+        return authorRepository.fetchSliceSortWhere(30, PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public List<Author> fetchAllAuthorsNative() {
+        return authorRepository.fetchAllNative();
+    }
+    
+    public Author fetchAuthorByNameAndAgeNative() {
+        return authorRepository.fetchByNameAndAgeNative("Joana Nimar", 34);
+    }
+    
+    /* causes exception
+    public List<Author> fetchAuthorsViaSortNative() {
+        return authorRepository.fetchViaSortNative(Sort.by(Direction.DESC, "name"));
+    }
+    */
+    
+    /* causes exception
+    public List<Author> fetchAuthorsViaSortWhereNative() {
+        return authorRepository.fetchViaSortWhereNative(30, Sort.by(Direction.DESC, "name"));
+    }
+    */
+    
+    public Page<Author> fetchAuthorsPageSortNative() {
+        return authorRepository.fetchPageSortNative(PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public Page<Author> fetchAuthorsPageSortWhereNative() {
+        return authorRepository.fetchPageSortWhereNative(30, PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    public Slice<Author> fetchAuthorsSliceSortNative() {
+        return authorRepository.fetchSliceSortNative(PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+    
+    public Slice<Author> fetchAuthorsSliceSortWhereNative() {
+        return authorRepository.fetchSliceSortWhereNative(30, PageRequest.of(1, 3,
+                Sort.by(Sort.Direction.DESC, "name")));
+    }
+
 }
