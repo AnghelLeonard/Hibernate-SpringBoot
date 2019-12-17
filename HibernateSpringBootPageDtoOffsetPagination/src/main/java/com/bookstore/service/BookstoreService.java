70acd9093dc0b1616046cf77a772fc3aa9a20bf9
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-17 17:01:25 +0200

    SELECT COUNT subquery + Page<DTO>

--- HibernateSpringBootPageDtoOffsetPagination/src/main/java/com/bookstore/service/BookstoreService.java
+++ HibernateSpringBootPageDtoOffsetPagination/src/main/java/com/bookstore/service/BookstoreService.java
@@ -20,7 +20,7 @@ public class BookstoreService {
     }
 
     public Page<AuthorDto> fetchNextPageNative(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, new Sort(Sort.Direction.ASC, "age"));
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "age"));
 
         List<AuthorDto> authors = authorRepository.fetchAllNative(pageable);
         Page<AuthorDto> pageOfAuthors = new PageImpl(authors, pageable,
@@ -30,7 +30,7 @@ public class BookstoreService {
     }
 
     public Page<AuthorDto> fetchNextPageJpql(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, new Sort(Sort.Direction.ASC, "age"));
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "age"));
 
         List<AuthorDto> authors = authorRepository.fetchAllJpql(pageable);
         Page<AuthorDto> pageOfAuthors = new PageImpl(authors, pageable,
