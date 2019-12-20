b7753d17470ae28f9d46bea32334db208118cd83
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-20 17:45:26 +0200

    Best way to use @transactional

--- HibernateSpringBootTransactionalInRepository/src/main/java/com/bookstore/entity/Author.java
+++ HibernateSpringBootTransactionalInRepository/src/main/java/com/bookstore/entity/Author.java
@@ -2,8 +2,6 @@ package com.bookstore.entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,8 +9,7 @@ public class Author implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id    
     private Long id;
 
     private int age;
