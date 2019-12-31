29091823bde7fa4709fe39946d2f53256e043f56
AnghelLeonard <leoprivacy@yahoo.com>
2019-12-31 08:34:35 +0200

    Stream data in MySQL

--- HibernateSpringBootStreamAndMySQL/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootStreamAndMySQL/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -15,4 +15,10 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     @Query("SELECT a FROM Author a")
     @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "" + Integer.MIN_VALUE))
     Stream<Author> streamAll();
+    
+    /* use this method if you add to JDBC URL the useCursorFetch=true setting
+    @Query("SELECT a FROM Author a")
+    @QueryHints(value = @QueryHint(name = HINT_FETCH_SIZE, value = "30"))
+    Stream<Author> streamAll();
+    */
 }
