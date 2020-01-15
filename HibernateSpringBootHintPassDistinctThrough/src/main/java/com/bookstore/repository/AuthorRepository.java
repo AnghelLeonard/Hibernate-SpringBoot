65182763dc3547956360996c07446924f709a16e
AnghelLeonard <leoprivacy@yahoo.com>
2020-01-15 09:30:24 +0200

    HINT_PASS_DISTINCT_THROUGH

--- HibernateSpringBootHintPassDistinctThrough/src/main/java/com/bookstore/repository/AuthorRepository.java
+++ HibernateSpringBootHintPassDistinctThrough/src/main/java/com/bookstore/repository/AuthorRepository.java
@@ -11,17 +11,15 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    @Transactional(readOnly = true)
+    
     @Query("SELECT a FROM Author a LEFT JOIN FETCH a.books")
     List<Author> fetchWithDuplicates();
 
-    @Transactional(readOnly = true)
     @Query("SELECT DISTINCT a FROM Author a LEFT JOIN FETCH a.books")
     List<Author> fetchWithoutHint();
 
-    @Transactional(readOnly = true)
     @Query("SELECT DISTINCT a FROM Author a LEFT JOIN FETCH a.books")
     @QueryHints(value = @QueryHint(name = HINT_PASS_DISTINCT_THROUGH, value = "false"))
     List<Author> fetchWithHint();
